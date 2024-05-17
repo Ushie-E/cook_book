d85587153738c199eddd2062e0c9f4e4692ae410
Emmanuel <adarikushie00@gmail.com>
2024-05-17 21:59:53 +0100

    Update string app

--- lib/ui/common/app_strings.dart
+++ lib/ui/common/app_strings.dart
@@ -1,3 +1,17 @@
 const String ksHomeBottomSheetTitle = 'Build Great Apps!';
 const String ksHomeBottomSheetDescription =
     'Stacked is built to help you build better apps. Give us a chance and we\'ll prove it to you. Check out stacked.filledstacks.com to learn more';
+const String kTitlePainterCookBook = "Painter Canvas";
+const String kSubTitlePainterCookBook =
+    "Drawing and Painting using CustomPainter";
+const String kTitlePDFCookBook = "Loading PDF";
+const String kSubTitlePDFCookBook = "Loading PDF file from Firebase Storage";
+const String kTitleThemeCookBook = "Theme Caching (Moor)";
+const String kSubTitleThemeCookBook = "Save Current Theme in Local database";
+const String kTitleActionCookBook = "Theme in Action";
+const String kSubTitleActionCookBook = "Toggle between Light & Dark themes";
+const String kTitlePluginCookBook = "Text-To-Speech Plugin";
+const String kSubTitlePluginCookBook =
+    "Code recipe to demonstrate usage of it Flutter Plugin";
+const String kTitleCookBook = "";
+const String kSubTitleCookBook = "";
