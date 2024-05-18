4aa5aa636ae6ffe34824c7c79280cc2c987369aa
Emmanuel <adarikushie00@gmail.com>
2024-05-18 10:31:39 +0100

    Function painter

--- lib/ui/views/painter/painter_viewmodel.dart
+++ lib/ui/views/painter/painter_viewmodel.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: unnecessary_null_comparison
-
-import 'dart:ui';
 import 'package:cook_book/app/app.dialogs.dart';
 import 'package:cook_book/app/app.locator.dart';
+import 'package:cook_book/ui/common/ui_helpers.dart';
+import 'package:cook_book/ui/views/painter/component/touch_point.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -10,13 +9,8 @@ import 'package:stacked_services/stacked_services.dart';
 class PainterViewModel extends BaseViewModel {
   final _dialogService = locator<DialogService>();
 
-  List<TouchPoints> get points => [];
-
-  void start(DragStartDetails details) {}
-
-  void update(DragUpdateDetails details) {}
-
-  void end(DragEndDetails details) {}
+  final List<TouchPoints> _points = [];
+  List<TouchPoints> get points => _points;
 
   bool _isVisible = false;
   bool get isVisible => _isVisible;
@@ -26,58 +20,86 @@ class PainterViewModel extends BaseViewModel {
     rebuildUi();
   }
 
-  void opacity() {
-    _dialogService.showCustomDialog(
+  void start(DragStartDetails details, BuildContext context) {
+    RenderBox renderBox = context.findRenderObject() as RenderBox;
+    _points.add(
+      TouchPoints(
+        points: renderBox.globalToLocal(details.globalPosition),
+        paint: Paint()
+          ..strokeCap = strokeType
+          ..isAntiAlias = true
+          ..color = selectedColor.withOpacity(opacities)
+          ..strokeWidth = strokeWidth,
+      ),
+    );
+    rebuildUi();
+  }
+
+  void update(DragUpdateDetails details, BuildContext context) {
+    RenderBox renderBox = context.findRenderObject() as RenderBox;
+    _points.add(
+      TouchPoints(
+        points: renderBox.globalToLocal(details.globalPosition),
+        paint: Paint()
+          ..strokeCap = strokeType
+          ..isAntiAlias = true
+          ..color = selectedColor.withOpacity(opacities)
+          ..strokeWidth = strokeWidth,
+      ),
+    );
+    rebuildUi();
+  }
+
+  void end(DragEndDetails details) {
+    points.add;
+    rebuildUi();
+  }
+
+  Future<void> opacity() async {
+    var response = await _dialogService.showCustomDialog(
       variant: DialogType.opacity,
+      barrierDismissible: true,
     );
-  }
-
-  void pickingRed() {}
-
-  void pickingGreen() {}
-
-  void pickingPink() {}
-
-  void pickingBlue() {}
-
-  void stroke() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.pickStroke,
-    );
-  }
-}
-
-class TouchPoints {
-  Paint paint;
-  Offset points;
-  TouchPoints({required this.points, required this.paint});
-}
-
-class MyPainter extends CustomPainter {
-  MyPainter({required this.pointsList});
-
-  List<TouchPoints> pointsList;
-  List<Offset> offsetPoints = [];
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (int i = 0; i < pointsList.length - 1; i++) {
-      if (pointsList[i] != null && pointsList[i + 1] != null) {
-        //Drawing line when two consecutive points are available
-        canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
-            pointsList[i].paint);
-      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
-        offsetPoints.clear();
-        offsetPoints.add(pointsList[i].points);
-        offsetPoints.add(Offset(
-            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
-
-        //Draw points when two points are not next to each other
-        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
-      }
+    if (response != null && response.confirmed) {
+      opacities = response.data;
+      rebuildUi();
     }
   }
 
-  @override
-  bool shouldRepaint(MyPainter oldDelegate) => true;
+  Future<void> stroke() async {
+    var response = await _dialogService.showCustomDialog(
+      variant: DialogType.pickStroke,
+      barrierDismissible: true,
+    );
+
+    if (response != null && response.confirmed) {
+      strokeWidth = response.data;
+      rebuildUi();
+    }
+  }
+
+  void pickingRed() {
+    selectedColor = Colors.red;
+    rebuildUi();
+  }
+
+  void pickingGreen() {
+    selectedColor = Colors.green;
+    rebuildUi();
+  }
+
+  void pickingPink() {
+    selectedColor = Colors.pink;
+    rebuildUi();
+  }
+
+  void pickingBlue() {
+    selectedColor = Colors.blue;
+    rebuildUi();
+  }
+
+  void erase() {
+    points.clear();
+    rebuildUi();
+  }
 }
