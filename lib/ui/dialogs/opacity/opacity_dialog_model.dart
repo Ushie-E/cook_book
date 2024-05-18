bd3c81c975cf63c2ea75e3ca68ce7693fc937e4e
Emmanuel <adarikushie00@gmail.com>
2024-05-18 10:27:28 +0100

    delete dialog view Model

--- lib/ui/dialogs/opacity/opacity_dialog_model.dart
+++
@@ -1,3 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class OpacityDialogModel extends BaseViewModel {}
--- lib/ui/dialogs/pick_stroke/pick_stroke_dialog_model.dart
+++
@@ -1,3 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class PickStrokeDialogModel extends BaseViewModel {}
